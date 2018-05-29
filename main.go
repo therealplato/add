7af61b3dfe062e4b6886353a4fcc84139f361ca3
Isaac Rogers <therealplato@gmail.com>
2018-05-29 13:07:40 +1200

    add: handle non-newline-terminated input

--- main.go
+++ main.go
@@ -12,27 +12,26 @@ import (
 
 func main() {
 	total := 0
-	_ = total
 
 	r := bufio.NewReader(os.Stdin)
 	for {
 		l, err := r.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
+		if err != nil && err != io.EOF {
 			log.Fatal(err)
 		}
 		l = strings.TrimSpace(l)
 		if len(l) == 0 {
 			continue
 		}
-		n, err := strconv.Atoi(l)
-		if err != nil {
+		n, err2 := strconv.Atoi(l)
+		if err2 != nil {
 			fmt.Fprintf(os.Stdout, "%q was not an integer, skipping\n", l)
 			continue
 		}
 		total += n
+		if err == io.EOF {
+			break
+		}
 	}
 	fmt.Println(total)
 }
